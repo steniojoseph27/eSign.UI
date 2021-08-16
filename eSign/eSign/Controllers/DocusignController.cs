f289172824797ae4ccee19e051725c81c390abdc
steniojoseph72 <steniojoseph@yahoo.com>
2021-08-15 23:21:29 -0400

    Added model and controller classes

---
+++ eSign/eSign/Controllers/DocusignController.cs
@@ -0,0 +1,136 @@
+﻿using DocuSign.eSign.Api;
+using DocuSign.eSign.Client;
+using DocuSign.eSign.Model;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using Document = DocuSign.eSign.Model.Document;
+
+namespace DocusignDemo.Controllers
+{
+    public class DocusignController : Controller
+    {
+        MyCredential credential = new MyCredential();
+        private string INTEGRATOR_KEY = "b2b84718-bebf-42eb-84e5-a01f91f3a71e";
+
+
+        public ActionResult SendDocumentforSign()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendDocumentforSign(Document document, HttpPostedFileBase UploadDocument)
+        {
+            Document recipientModel = new Document();
+            string directorypath = Server.MapPath("~/App_Data/" + "Files/");
+            if (!Directory.Exists(directorypath))
+            {
+                Directory.CreateDirectory(directorypath);
+
+            }
+
+            byte[] data;
+            using (Stream inputStream = UploadDocument.InputStream)
+            {
+                MemoryStream memoryStream = inputStream as MemoryStream;
+                if (memoryStream == null)
+                {
+                    memoryStream = new MemoryStream();
+                    inputStream.CopyTo(memoryStream);
+                }
+                data = memoryStream.ToArray();
+            }
+
+            var serverpath = directorypath + document.Name.Trim() + ".pdf";
+            System.IO.File.WriteAllBytes(serverpath, data);
+            docusign(serverpath, document.Name, document.Email);
+            return View();
+        }
+
+        public string loginApi(string usr, string pwd)
+        {
+            // we set the api client in global config when we configured the client 
+            ApiClient apiClient = Configuration.Default.ApiClient;
+            string authHeader = "{\"Username\":\"" + usr + "\", \"Password\":\"" + pwd + "\", \"IntegratorKey\":\"" + INTEGRATOR_KEY + "\"}";
+            Configuration.Default.AddDefaultHeader("X-DocuSign-Authentication", authHeader);
+
+            // we will retrieve this from the login() results
+            string accountId = null;
+
+            // the authentication api uses the apiClient (and X-DocuSign-Authentication header) that are set in Configuration object
+            AuthenticationApi authApi = new AuthenticationApi();
+            LoginInformation loginInfo = authApi.Login();
+
+            // find the default account for this user
+            foreach (DocuSign.eSign.Model.LoginAccount loginAcct in loginInfo.LoginAccounts)
+            {
+                if (loginAcct.IsDefault == "true")
+                {
+                    accountId = loginAcct.AccountId;
+                    break;
+                }
+            }
+            if (accountId == null)
+            { // if no default found set to first account
+                accountId = loginInfo.LoginAccounts[0].AccountId;
+            }
+            return accountId;
+        }
+
+        public void docusign(string path, string documentName, string documentEmail)
+        {
+
+            ApiClient apiClient = new ApiClient("https://demo.docusign.net/restapi");
+            Configuration.Default.ApiClient = apiClient;
+
+            //Verify Account Details
+            string accountId = loginApi(credential.UserName, credential.Password);
+
+            // Read a file from disk to use as a document.
+            byte[] fileBytes = System.IO.File.ReadAllBytes(path);
+
+            EnvelopeDefinition envDef = new EnvelopeDefinition();
+            envDef.EmailSubject = "Please sign this doc";
+
+            // Add a document to the envelope
+            Document doc = new Document();
+            doc.DocumentBase64 = System.Convert.ToBase64String(fileBytes);
+            doc.Name = Path.GetFileName(path);
+            doc.DocumentId = "1";
+
+            envDef.Documents = new List<Document>();
+            envDef.Documents.Add(doc);
+
+            // Add a recipient to sign the documeent
+            DocuSign.eSign.Model.Signer signer = new DocuSign.eSign.Model.Signer();
+            signer.Email = documentEmail;
+            signer.Name = documentName;
+            signer.RecipientId = "1";
+
+            envDef.Recipients = new DocuSign.eSign.Model.Recipients();
+            envDef.Recipients.Signers = new List<DocuSign.eSign.Model.Signer>();
+            envDef.Recipients.Signers.Add(signer);
+
+            //set envelope status to "sent" to immediately send the signature request
+            envDef.Status = "sent";
+
+            // |EnvelopesApi| contains methods related to creating and sending Envelopes (aka signature requests)
+            EnvelopesApi envelopesApi = new EnvelopesApi();
+            EnvelopeSummary envelopeSummary = envelopesApi.CreateEnvelope(accountId, envDef);
+
+            // print the JSON response
+            var result = JsonConvert.SerializeObject(envelopeSummary);
+        }
+    }
+
+
+
+    public class MyCredential
+    {
+        public string UserName { get; set; } = "steniojoseph@yahoo.com";
+        public string Password { get; set; } = "smkg@lance1";
+    }
+}
---
+++ eSign/eSign/Models/Document.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eSign.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Email { get; set; }
+        public string Signatories { get; set; }
+        public DateTime creationDate { get; set; }
+        public string Description { get; set; }
+    }
+}
